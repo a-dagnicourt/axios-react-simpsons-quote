14e1807cb242273b8ef276f31ee84813dd40e1fe
a-dagnicourt <dagnicourt.a@gmail.com>
2020-10-26 15:04:44 +0100

    re-added original API call name + added componentDidMount and ternary for loading

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import { Component } from 'react';
 import './App.css';
-import QuoteCard from './components/QuoteCard';
 import axios from 'axios';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
+import QuoteCard from './components/QuoteCard';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      simpson: [],
+      simpson: null,
     };
-    this.componentDidMount = this.componentDidMount.bind(this);
+    this.randomQuote = this.randomQuote.bind(this);
   }
 
-  componentDidMount() {
+  randomQuote() {
     axios.get('https://thesimpsonsquoteapi.glitch.me/quotes').then((res) => {
       const simpson = res.data[0];
       this.setState({
@@ -21,17 +23,24 @@ class App extends Component {
     });
   }
 
+  componentDidMount() {
+    this.randomQuote();
+  }
+
   render() {
     return (
       <div className='App'>
         <h1>The Simpsons Quotes Generator</h1>
-        <button
-          className='myButton'
-          type='button'
-          onClick={this.componentDidMount}>
+        <button className='myButton' type='button' onClick={this.randomQuote}>
           Randomize
         </button>
-        <QuoteCard simpson={this.state.simpson} />
+        {this.state.simpson ? (
+          <QuoteCard simpson={this.state.simpson} />
+        ) : (
+          <div>
+            <FontAwesomeIcon icon={faSpinner} spin /> Loading...
+          </div>
+        )}
       </div>
     );
   }
